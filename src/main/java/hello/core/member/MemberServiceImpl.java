c306a94389706b6cb6cd1575eb6950bdb5ad60d7
김민지 <9mjkim@gmail.com>
2022-05-17 16:13:32 +0900

    style: 주석 추가
    MemberServiveImpl.java 에 코드 문제점 작성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,10 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /**
+     * MemberService가 역할 부분인 MemberRepository, 그리고 구현 부분인 MemoryMemberRepository 모두 의존하고 있다.
+     * 이는 DIP 위반이고, 이후 변경이 있을 때도 문제가 생길 수 있다.
+     */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
