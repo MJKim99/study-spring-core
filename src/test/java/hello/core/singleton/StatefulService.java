5276071780c232302963eb54eb87ec078dfa640f
김민지 <9mjkim@gmail.com>
2024-03-05 11:08:57 +0900

    feat: 싱글턴 방식 주의점 해결

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -4,9 +4,15 @@ public class StatefulService {
 
     private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + ", price = " + price);
+//        this.price = price; // 문제
+//    }
+
+    public int order(String name, int price) {
         System.out.println("name = " + name + ", price = " + price);
-        this.price = price; // 문제
+        this.price = price;
+        return price;
     }
 
     public int getPrice() {
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,16 +17,19 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
-        // ThreadA : 사용자 A 10000원 주문
-        statefulService1.order("userA", 10000);
-        // ThreadB : 사용자 B 10000원 주문
-        statefulService2.order("userB", 20000);
+//        // ThreadA : 사용자 A 10000원 주문
+//        statefulService1.order("userA", 10000);
+//        // ThreadB : 사용자 B 10000원 주문
+//        statefulService2.order("userB", 20000);
+
+        int userAPrice = statefulService1.order("userA", 10000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // ThreadA : 사용자 A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
